549748a2fc7cf12e86c31400da6fac0bfe6127f5
devski571 <dev.sundhoo@nhs.net>
2025-12-30 23:02:10 +0000

    Update streamlit_app.py
    adding .json to end of the response back, so it will format as json

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,6 +71,4 @@ if ingredients_List:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
-              
-
+st.text(smoothiefroot_response.json())
